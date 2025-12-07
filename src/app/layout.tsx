89dbb3fe01605b4ffa239bf7be693ac17a730db0
MichaelRommm <mih.kuf@gmail.com>
2025-12-07 10:06:14 +0200

    Add Google Analytics scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const rubik = Rubik({
@@ -25,6 +26,18 @@ export default function RootLayout({
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 			</head>
 			<body className={`${rubik.variable} font-rubik antialiased`}>
+				<Script
+					strategy="afterInteractive"
+					src="https://www.googletagmanager.com/gtag/js?id=G-5C3WHD036H"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-5C3WHD036H');
+					`}
+				</Script>
 				{children}
 			</body>
 		</html>
